125cbdba1a38cb91c7ff2dea2fcae966b43fb5a0
Ewan Langlais <ewanlanglais@air-de-ewan.home>
2025-10-30 16:59:10 +0100

    diagnostic: minimal webhook

--- api/webhook/[secret].js
+++ api/webhook/[secret].js
@@ -1,13 +1,4 @@
-const { webhookCallback } = require('grammy');
-const bot = require('../_bot');
-
 module.exports = (req, res) => {
-  // Répondre 200 sur GET (ping Telegram)
-  if (req.method === 'GET') {
-    res.status(200).send('ok');
-    return;
-  }
-  // Passer toutes les requêtes POST au bot, sans check
-  const handle = webhookCallback(bot, 'http');
-  return handle(req, res);
+  console.log('WEBHOOK', { method: req.method, headers: req.headers, body: req.body });
+  res.status(200).end('ok');
 };
